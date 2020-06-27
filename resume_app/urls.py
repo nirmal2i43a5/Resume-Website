3a2de8f5488dbe3d0ac3e025c1f30fd13fd6a40e
Nirmal <nirmalpandey048@gmail.com>
2020-06-27 21:37:12 +0545

    changes

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from resume_app import views
 
 
+
+
 app_name = 'resume_app'
 
 urlpatterns = [
-    path('',views.home,name ="home")
+    path('',views.home,name ="home"),
+ 
 ]
